c69dfddb27ad6b1ad44c0a6fef336c57e6d5b00b
dantran16 <trand4@oregonstate.edu>
2023-07-14 16:57:38 -0400

    Added info banner for new feature

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Games from './pages/Games';
 import Bookmarks from './pages/Bookmarks';
+import NewFeature from './components/NewFeature';
 
 function App() {
   return (
     <BrowserRouter>
       <Container className='App' mt={3} maxWidth="container.xl">
         <Header />
+        <NewFeature />
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/bookmarks" element={<Bookmarks />}></Route>
---
+++ src/components/NewFeature.js
@@ -0,0 +1,31 @@
+import { Box, Alert, 
+    AlertIcon,
+    AlertTitle,
+    AlertDescription, CloseButton, useDisclosure, Button } from "@chakra-ui/react";
+
+
+const NewFeature = () => {
+    const {
+        isOpen: isVisible,
+        onClose,
+        onOpen
+    } = useDisclosure({defaultIsOpen: true})
+
+    return isVisible ? (
+        <Alert status="info" p={4} my={4} fontSize={18} maxWidth={"container.xl"}>
+            <AlertIcon />
+            <Box width={'100%'}>
+                <AlertTitle>New Feature - Bookmarks</AlertTitle>
+                <AlertDescription>You can now bookmark tips, so you can view them at any point.</AlertDescription>
+            </Box>
+            <CloseButton
+                alignSelf='flex-start'
+                position='relative'
+                right={-1}
+                top={-1}
+                onClick={onClose}
+            />
+        </Alert>) : (<Button onClick={onOpen}>Show Update</Button>)
+}
+
+export default NewFeature
