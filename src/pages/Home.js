1d0f6de68bfd8da52905a55a81d6c24ea0866a00
dantran16 <trand4@oregonstate.edu>
2023-07-17 17:48:04 -0400

    Added Ordered List for explicit instructions

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,27 @@
-import { Box, Text} from "@chakra-ui/react";
+import { Box, OrderedList, Text, ListItem } from "@chakra-ui/react";
 import HeadingOne from "../components/HeadingOne";
 import GenerateForm from "../components/GenerateForm";
 import TipCard from "../components/TipCard";
 
+const Home = ({ tipHistory, setTipHistory }) => {
+  const renderTips = () =>
+    tipHistory.map((tip) => <TipCard tip={tip} key={tip.id} />);
 
-const Home = ({tipHistory, setTipHistory}) => {
-
-    const renderTips = () => tipHistory.map(tip => (
-        <TipCard tip={tip} key={tip.id}/>
-    ))
-
-    return (
+  return (
     <Box p={2} className="home">
-        <HeadingOne>Random Gaming Tip Generator</HeadingOne>
-        <Text px={1} my={2}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Text>
-        <GenerateForm setTipHistory={setTipHistory} />
-        <Box>
-            {tipHistory.length > 0 && renderTips()}
-        </Box>
+      <HeadingOne>Random Gaming Tip Generator</HeadingOne>
+      <Text px={1} py={2} my={2}>
+        Generate a random gaming tip based on the game.
+      </Text>
+      <OrderedList px={4} my={2}>
+        <ListItem>Select a game.</ListItem>
+        <ListItem>To filter more, click on Filter Options.</ListItem>
+        <ListItem>Click on Generate Gaming Tip.</ListItem>
+      </OrderedList>
+      <GenerateForm setTipHistory={setTipHistory} />
+      <Box p={2}>{tipHistory.length > 0 && renderTips()}</Box>
     </Box>
-)}
+  );
+};
 
 export default Home;
